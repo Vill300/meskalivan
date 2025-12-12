dd6a39db4a70027d5c5d2c7570f0264223d4af24
Vill300 <hazder1no@vk.com>
2025-12-13 02:13:30 +0300

    Fix GitHub Pages deployment - white screen issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/meskalivan/',
   build: {
     outDir: 'dist',
-    sourcemap: true
-  }
+    sourcemap: false
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:5173',
+  },
 })
